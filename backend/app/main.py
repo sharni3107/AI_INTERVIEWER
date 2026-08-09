a55aee2b5264e9d123faf4e6a3892d5d0c8b2fb0
sharni3107 <sharnitha3107@gmail.com>
2026-08-09 20:32:25 +0530

    feat:final

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,10 +11,12 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
+  
+allow_origins=[
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-],
+    "https://ai-interviewer-svhw.vercel.app",
+]
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
