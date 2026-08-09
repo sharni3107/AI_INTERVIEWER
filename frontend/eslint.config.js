4a9ccdf494a920e246cc61c8f21ad6f6d0200909
sharni3107 <sharnitha3107@gmail.com>
2026-08-09 18:38:27 +0530

    chore: complete project integration and configuration

---
+++ frontend/eslint.config.js
@@ -0,0 +1,29 @@
+import js from '@eslint/js'
+import react from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+
+export default [
+  { ignores: ['dist'] },
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      parserOptions: { ecmaFeatures: { jsx: true } },
+      globals: { window: 'readonly', document: 'readonly', sessionStorage: 'readonly', fetch: 'readonly', crypto: 'readonly' },
+    },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      'react/jsx-uses-react': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'no-unused-vars': 'warn',
+    },
+  },
+]
